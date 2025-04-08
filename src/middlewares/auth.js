a64e32766c283361bcdeef04fd5cd438c8cb0af8
Javeriach <javeriakanwal383@gmail.com>
2025-04-09 01:49:43 +0500

    Feat:update vercel

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,22 @@
-const jwt = require("jsonwebtoken");
-const { User } = require("../models/user");
+const jwt = require('jsonwebtoken');
+const { User } = require('../models/user');
 const userAuth = async (req, res, next) => {
   try {
     //read token from the cookies (attach with request)
     const { token } = req.cookies;
 
     // validate the token
-    if (!token) return res.status(401).json({ message: "Login again" });
+    if (!token) return res.status(401).json({ message: 'Login again' });
     // find the user
     const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
     const { _id } = decodedObj;
     const user = await User.findById(_id);
-    if (!user) throw new Error("User not found");
-
+    if (!user) throw new Error('User not found');
+    //sfd
     req.user = user;
     next();
   } catch (err) {
-    res.status(400).send("ERROR : " + err.message);
+    res.status(400).send('ERROR : ' + err.message);
   }
 };
 
